e136991340451cb563a8862774a2d96d4d739355
Constance Nwaigwe <constance.nwaigwe@aura.com>
2022-03-21 00:46:41 -0400

    added css and data

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -9,21 +9,26 @@ function App() {
           src="https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg"
           alt="Cartoon food"
         />
-        <h1>FOOD BLOG</h1>
+        <h1>FOOD FACTS</h1>
         <img
           src="https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg"
           alt="Cartoon food"
         />
       </header>
       <section>
-        <p>Body</p>
-        <div>
+        <div className="cards">
           {recipeData.map((data) => {
             return (
-              <div>
-                <img src={data.image} />
-                <p>{data.name}</p>
-                <p>{data.items}</p>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src={data.image} className="cardimg" />
+                    <p>{data.name}</p>
+                  </div>
+                  <div className="flip-card-back">
+                    <p>{data.items}</p>
+                  </div>
+                </div>
               </div>
             );
           })}
--- react-website/src/components/data.js
+++ react-website/src/components/data.js
@@ -1,33 +1,46 @@
 const recipeData = [
   {
     image:
-      "https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg",
+      "https://www.cookingclassy.com/wp-content/uploads/2021/01/butter-chicken-4.jpg",
     name: "Butter Chicken",
-    items: "one, two, three",
+    items:
+      "Butter chicken, traditionally known as Murgh makhani, is a type of curry made from chicken with a spiced tomato and butter sauce. Its sauce is known for its rich texture. In addition sometimes the cream gives the curry sauce a silky smooth rich texture. It originated in the Indian subcontinent as a curry.",
   },
   {
     image:
-      "https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg",
+      "https://www.cookingclassy.com/wp-content/uploads/2020/04/banana-pancakes-18.jpg",
     name: "Banana Pancake",
-    items: "one, two, three",
+    items:
+      "These pancakes are gluten-free and dairy-free — they really are made with just bananas and eggs, no tricks. It seems like you'd just end up with scrambled banana-eggs (yuck), but the final product truly resembles a pancake. Piping hot and golden brown, with crispy edges.",
   },
   {
     image:
-      "https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg",
+      "https://bakingmischief.com/wp-content/uploads/2020/03/vanilla-milkshake-image-square.jpg",
     name: "Vanila Milkshake",
-    items: "one, two, three",
+    items:
+      "Making classic vanilla milkshakes at home is really easy! Only three ingredients: vanilla ice cream, milk, and vanilla extract. I'll share a few tips to make the perfect milkshakes at home. The great thing about making milkshakes at home is that you have full control of the type and quality of ice cream you use.",
   },
   {
     image:
-      "https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/macaroni-and-cheese-recipe-1622135326.png?crop=0.786xw:0.901xh;0.0731xw,0.00975xh&resize=640:*",
     name: "Mac n Cheese",
-    items: "one, two, three",
+    items:
+      "Macaroni and cheese—also called mac and cheese in the United States and macaroni cheese in the United Kingdom—is a dish of cooked macaroni pasta and a cheese sauce, most commonly cheddar. It can also incorporate other ingredients, such as breadcrumbs or meat.",
   },
   {
     image:
-      "https://www.arabnews.jp/en/wp-content/uploads/sites/2/2021/04/WhatsApp-Image-2021-04-13-at-1.33.15-PM-1.jpeg",
-    name: "Shrimp Afrado",
-    items: "",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR89ONA5CWdeS8Alu4Kz7KKO7TvLq478r4_Lg&usqp=CAU",
+    name: "Jollof Rice",
+    items:
+      "Jollof, or jollof rice, is a rice dish from West Africa. The dish is typically made with long-grain rice, tomatoes, onions, spices, vegetables and meat in a single pot, although its ingredients and preparation methods vary across different regions.",
+  },
+
+  {
+    image:
+      "https://yelskitchen.com/wp-content/uploads/2020/07/839C3AE2-F45E-427C-92E4-B693DCB75B2E-B0D3935B-4A6D-4C05-808B-B343D6C592F1-1140x1520.jpg",
+    name: "Shrimp Alfredo",
+    items:
+      "A traditional Alfredo sauce calls for pepper that is freshly ground from peppercorns. However, black pepper that is already ground or even white pepper are options as well. The complementary spices you may wish to include are lemon juice, garlic and even nutmeg.",
   },
 ];
 
